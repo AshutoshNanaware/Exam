d0a66edcdd697be009d645da00adcc6c62f5c093
ashu <ashutoshnanaware777@gmail.com>
2021-12-28 09:04:19 +0530

    initial commit

---
+++ reactTemp/Pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from "./cnvert.jpg"
+
+function Home() {
+        
+
+    return (
+        <div>
+
+
+            <h1 className='mb-3'> ------Shree Hospital------ </h1><br/>
+            
+            <img className='logo' src={logo} alt="LOGO"  width="1600" height="400"  ></img>
+          
+            </div>
+    )
+}
+
+export default Home
